82ac5f9abdf0180c7a733d76959f9c29ddf5d10b
Lennoxobu <lennoxobu@gmail.com>
2025-03-12 19:04:38 +0000

    Added SavingAccountService methods with SavingAccountRepository methods phase 1 before testing

--- src/main/java/com/example/BankingAppCRUD/Account/SavingAccountRepository.java
+++ src/main/java/com/example/BankingAppCRUD/Account/SavingAccountRepository.java
@@ -1,8 +1,29 @@
 package com.example.BankingAppCRUD.Account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SavingAccountRepository extends JpaRepository<SavingAccount, Long> {
 
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE _SavingAccount SET firstName = :keyword WHERE id = :id",
+            nativeQuery = true)
+    int updateByFirstName (@Param("keyword") String value , @Param("id") long id );
 
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE _SavingAccount SET lastName= :keyword WHERE id = :id",
+            nativeQuery = true)
+    int updateByLastName (@Param("keyword") String value, @Param("id") long id);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE  _SavingAccount SET balance = :Num WHERE id = :id",
+            nativeQuery = true)
+    int updateByBalance (@Param("Num") double Num , @Param("id") double id);
 }
--- src/main/java/com/example/BankingAppCRUD/Account/SavingAccountService.java
+++ src/main/java/com/example/BankingAppCRUD/Account/SavingAccountService.java
@@ -1,5 +1,6 @@
 package com.example.BankingAppCRUD.Account;
 
+import com.example.BankingAppCRUD.config.Utils.InterestRate.InterestRateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,14 +13,18 @@ public class SavingAccountService implements AccountService<SavingAccount> {
     @Autowired
     private final SavingAccountRepository savingAccountRepository;
 
+    @Autowired
+    private final InterestRateService interestRateService;
+
 
     @Autowired
-    SavingAccountService (SavingAccountRepository savingAccountRepository) {
+    SavingAccountService (SavingAccountRepository savingAccountRepository , InterestRateService interestRateService) {
         this.savingAccountRepository = savingAccountRepository;
+        this.interestRateService = interestRateService;
     }
 
 
-    // Checking Account Services Methods
+    // Saving Account Services Methods
     @Override
     public Page<SavingAccount> getAllAccounts (Pageable pageable) {
         return this.savingAccountRepository.findAll(pageable);
@@ -51,5 +56,76 @@ public class SavingAccountService implements AccountService<SavingAccount> {
 
 
     }
+
+
+    //Rate Business Methods
+    @Override
+    public double getRate(Long Id ) {
+        return this.savingAccountRepository.getReferenceById(Id).getRate();
+    }
+
+
+    @Override
+    public void setRate(Long Id, double value) {
+        this.savingAccountRepository.getReferenceById(Id).setRate(generateRate());
+    }
+
+    private  double generateRate() {
+
+        try {
+            return this.interestRateService.getInterestRate().block() + baseRate.rate;
+        } catch (NullPointerException Ex ) {
+            System.out.println(Ex.getMessage());
+            return baseRate.rate;
+
+        }
+
+
+    }
+
+    // Normal Banking Operations as per Account Service
+    @Override
+    public int deposit (double value , Long id  ) {
+
+        // Get Account
+
+        SavingAccount account = this.savingAccountRepository.getReferenceById(id);
+
+        value = account.getBalance() + value;
+
+        return this.savingAccountRepository.updateByBalance(value , id);
+    }
+
+
+    @Override
+    public int withdraw (double value , Long id ) throws Exception {
+        // Get Account
+        SavingAccount account  = this.savingAccountRepository.getReferenceById(id);
+
+        if (value <= account.getBalance()) {
+            value = account.getBalance() - value;
+
+            return this.savingAccountRepository.updateByBalance(value, id);
+        } else {
+
+            throw new Exception();
+        }
+
+    }
+
+    @Override
+    public int transfer (double value , Long receiverID,  Long grantorID ) throws Exception {
+        SavingAccount receiverAcc = this.savingAccountRepository.getReferenceById(receiverID);
+        SavingAccount grantorAcc = this.savingAccountRepository.getReferenceById(grantorID);
+
+        if (value <= grantorAcc.getBalance()) {
+            this.savingAccountRepository.updateByBalance(grantorAcc.getBalance() - value , grantorID);
+            return this.savingAccountRepository.updateByBalance(receiverAcc.getBalance() + value , receiverID);
+        } else {
+            throw new Exception();
+        }
+    }
+
+
 
 }
