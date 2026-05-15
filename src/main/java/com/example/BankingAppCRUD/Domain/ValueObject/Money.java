3e025a7ed2d1b65ce948757aa63ca886ec7027c7
Lennoxobu <lennoxobu@gmail.com>
2026-05-15 07:10:53 +0100

    Added MoneyInstantiator part 2 forgot to add the annotation on the VO

--- src/main/java/com/example/BankingAppCRUD/Domain/ValueObject/Money.java
+++ src/main/java/com/example/BankingAppCRUD/Domain/ValueObject/Money.java
@@ -1,11 +1,14 @@
 package com.example.BankingAppCRUD.Domain.ValueObject;
 
+import com.example.BankingAppCRUD.Domain.HibernateInstantiator.MoneyInstantiator;
+import com.example.BankingAppCRUD.Domain.HibernateInstantiator.NameInstantiator;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.EmbeddableInstantiator;
 
 @Data
 @Builder
@@ -14,6 +17,7 @@ import lombok.Data;
         @AttributeOverride(name = "money_amount" , column = @Column(name = "money_amount")),
         @AttributeOverride(name = "money_currency" , column = @Column(name = "money_currency"))
 })
+@EmbeddableInstantiator(MoneyInstantiator.class)
 public class Money {
 
     private final Long amount;
